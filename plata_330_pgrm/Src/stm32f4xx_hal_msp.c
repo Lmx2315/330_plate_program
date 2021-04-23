93ed8fbe91a4da05e9e5ce2853fcbf664f400083
lmx2315 <lmx2315@mail.ru>
2021-04-23 18:14:25 +0300

    zzz

--- plata_330_pgrm/Src/stm32f4xx_hal_msp.c
+++ plata_330_pgrm/Src/stm32f4xx_hal_msp.c
@@ -307,7 +307,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     PE9     ------> TIM1_CH1
     PE11     ------> TIM1_CH2 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_11;
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -358,6 +358,18 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
   /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
   /* USER CODE END TIM1_MspDeInit 1 */
+  } else
+	  
+    if(htim_base->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspDeInit 0 */
+
+  /* USER CODE END TIM2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM2_CLK_DISABLE();
+  /* USER CODE BEGIN TIM2_MspDeInit 1 */
+
+  /* USER CODE END TIM2_MspDeInit 1 */
   }
 
 }
